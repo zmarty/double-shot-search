c528f82e9627a04f476ccc615be00bb40a24f8a7
Ovidiu Dan <contact@ovidiudan.com>
2017-06-28 11:47:29 -0700

    Removed a console.log line

--- background.js
+++ background.js
@@ -28,7 +28,6 @@ chrome.omnibox.onInputEntered.addListener(
 
 function navigate(url) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log("navigated")
         chrome.tabs.update(tabs[0].id, {url: url});
     });
 }
