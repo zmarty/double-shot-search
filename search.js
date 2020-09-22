4f88783b452bab8a9a9a330948f531afc25c8267
Sarvesh Nagpal <sarveshnagpal@gmail.com>
2020-09-21 18:29:51 -0700

    Opening links in new tab

--- search.js
+++ search.js
@@ -1,13 +1,13 @@
 const shopPrefix = "shop ";
 $( document ).ready(function() {
-    let left = "https://www.bing.com/search?q=";
-    let right = "https://www.google.com/search?q=";
+    let left = "https://www.bing.com/search?ds=1&q=";
+    let right = "https://www.google.com/search?ds=1&q=";
     var url = $.url();
     var query = url.param("q");
 
     if (typeof query != 'undefined')
     {
-        if (query.indexOf(shopPrefix) === 0) {
+        if (query.toLowerCase().indexOf(shopPrefix) === 0) {
             query = query.substr(shopPrefix.length);
             left = left.replace("search?", "shop?");
             right = right.replace("search?", "search?psb=1&tbm=shop&");
